5ca3031fb5c6274715a6d78d4c8c31a39d7c8f4d
Akaydia <akaydiaaa@hotmail.com>
2023-01-22 17:00:45 -0500

    Added a start button, made logic to display the question 1 by 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 let quizEl = document.querySelector(".quiz-container");
-let mainEl = document.querySelector("main");
+let startBtn = document.getElementById("start-btn");
+let currentQuestionIndex = 0;
+let questionDivs = [];
+let resultDivs = [];
+let isAnswerClicked = false;
+
 const questions = [
     {
         question: "Commonly used data types DO NOT include:",
@@ -20,49 +25,80 @@ const questions = [
         ],
     },
 
-    
+
     // add more questions
 
 ];
 
+function moveToNextQuestion() {
+    // check if the answer is clicked and if the quiz is completed before moving to the next question
+    if (isAnswerClicked) {
+        if (currentQuestionIndex < questions.length - 1) {
+            currentQuestionIndex++;
+            questionDivs[currentQuestionIndex].style.display = "block";
+            resultDivs[currentQuestionIndex].style.display = "block";
+        } else {
+            console.log("Quiz completed =)");
+        }
+        isAnswerClicked = false;
+    }
+} // moveToNextQuestion()
 
+startBtn.addEventListener("click", function () {
+    questions.forEach((question, index) => {
+        // create a new div for the question and applies a "question" class to it
+        let questionDiv = document.createElement("div");
+        let resultDiv = document.createElement("div");
+        questionDiv.setAttribute("class", "question");
+        resultDiv.setAttribute("class", "result");
 
-questions.forEach((question) => {
-    // create a new div for the question and applies a "question" class to it
-    let questionDiv = document.createElement("div");
-    let resultDiv = document.createElement("div");
-    questionDiv.setAttribute("class", "question");
-    resultDiv.setAttribute("class", "result");
+        // create a new h2 for the question text
+        let questionText = document.createElement("h2");
+        questionText.innerText = question.question;
+        questionDiv.appendChild(questionText);
 
-    // create a new h2 for the question text
-    let questionText = document.createElement("h2");
-    questionText.innerText = question.question;
-    questionDiv.appendChild(questionText);
+        question.answers.forEach((answer) => {
 
-    question.answers.forEach((answer) => {
+            let answerDiv = document.createElement("div");
+            answerDiv.setAttribute("class", "answer");
+            answerDiv.innerText = answer.text;
+            answerDiv.dataset.correct = answer.correct;
 
-        let answerDiv = document.createElement("div");
-        answerDiv.setAttribute("class", "answer");
-        answerDiv.innerText = answer.text;
-        answerDiv.dataset.correct = answer.correct;
+            // adding eventlistener to elements in the div
+            answerDiv.addEventListener("click", (event) => {
+                isAnswerClicked = true;
+                //get element that was clicked
+                let selectedAnswer = event.target;
+                // compare if selected answer was correct
+                if (selectedAnswer.dataset.correct === "true") {
+                    resultDiv.innerText = "correct !! :)";
+                    resultDiv.setAttribute("class", "");
+                    resultDiv.setAttribute("class", "correct");
+                    moveToNextQuestion();
+                } else {
+                    resultDiv.innerText = "wrong !! lol";
+                    resultDiv.setAttribute("class", "");
+                    resultDiv.setAttribute("class", "wrong");
+                    moveToNextQuestion();
+                }
+            });
+            questionDiv.appendChild(answerDiv);
+        });
+        // added the created divs to an array of them, to keep track of their index
+        questionDivs.push(questionDiv);
+        resultDivs.push(resultDiv);
 
-        // adding eventlistener to elements in the div
-        answerDiv.addEventListener("click", (event) => {
-            //get element that was clicked
-            let selectedAnswer = event.target;
-            // compare if selected answer was correct
-            if (selectedAnswer.dataset.correct === "true") {
-                resultDiv.innerText = "correct !! :)";
-                resultDiv.setAttribute("class", "");
-                resultDiv.setAttribute("class", "correct");
-            } else {
-                resultDiv.innerText = "wrong !! lol";
-                resultDiv.setAttribute("class", "");
-                resultDiv.setAttribute("class", "wrong");
+        quizEl.appendChild(questionDiv);
+        questionDiv.appendChild(resultDiv);
+
+        // hide all question divs except the first one
+        questionDivs.forEach((div, i) => {
+            if (i !== currentQuestionIndex) {
+                div.style.display = "none";
+                resultDivs[i].style.display = "none";
             }
         });
-        questionDiv.appendChild(answerDiv);
-    });
-    quizEl.appendChild(questionDiv);
-    questionDiv.appendChild(resultDiv);
-}); // questions.forEach()
+        
+    }); // questions.forEach()
+    
+}); // startBtn.addEventListener("click") 
